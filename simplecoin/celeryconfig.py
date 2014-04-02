53df66df764b6a78dfd88b79a73e861bbab67e55
Musee Ullah <icelandisbeingcolouredbywrens@gmail.com>
2014-04-02 14:17:42 -0500

    revert simplecoin/celeryconfig.py to upstream

--- simplecoin/celeryconfig.py
+++ simplecoin/celeryconfig.py
@@ -4,11 +4,11 @@ from datetime import timedelta
 CELERYBEAT_SCHEDULE = {
     'cache_user_donations': {
         'task': 'simplecoin.tasks.cache_user_donation',
-        'schedule': timedelta(minutes=30)
+        'schedule': timedelta(minutes=15)
     },
     'compute_pplns': {
         'task': 'simplecoin.tasks.update_pplns_est',
-        'schedule': timedelta(minutes=1)
+        'schedule': timedelta(minutes=15)
     },
     'compress_min_shares': {
         'task': 'simplecoin.tasks.compress_minute',
@@ -16,11 +16,11 @@ CELERYBEAT_SCHEDULE = {
     },
     'compress_five_min_shares': {
         'task': 'simplecoin.tasks.compress_five_minute',
-        'schedule': timedelta(minutes=30),
+        'schedule': timedelta(minutes=60),
     },
     'general_cleanup': {
         'task': 'simplecoin.tasks.general_cleanup',
-        'schedule': timedelta(minutes=60),
+        'schedule': timedelta(minutes=120),
     },
     'update_block_state': {
         'task': 'simplecoin.tasks.update_block_state',
@@ -28,7 +28,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'server_status': {
         'task': 'simplecoin.tasks.server_status',
-        'schedule': timedelta(minutes=1),
+        'schedule': timedelta(minutes=2),
     },
     'check_worker_down': {
         'task': 'simplecoin.tasks.check_down',
@@ -36,7 +36,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'update_diff_average': {
         'task': 'simplecoin.tasks.difficulty_avg',
-        'schedule': timedelta(minutes=2),
+        'schedule': timedelta(hours=1),
     },
     'update_coin_trans': {
         'task': 'simplecoin.tasks.update_coin_transaction',
