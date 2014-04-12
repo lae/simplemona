e238b87ba03e6e41fff5e8bed44df714ec182538
Isaac Cook <isaac@simpload.com>
2014-04-11 20:17:25 -0500

    Abstracted user payout table to its own template and made a page to view all user transactions. Added sorting ability to transactions table.

--- simplecoin/models.py
+++ simplecoin/models.py
@@ -295,6 +295,10 @@ class Payout(Transfer):
         return payout
 
     @property
+    def timestamp(self):
+        return calendar.timegm(self.created_at.utctimetuple())
+
+    @property
     def status(self):
         if self.transaction:
             if self.transaction.confirmed is True:
